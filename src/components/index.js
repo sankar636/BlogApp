a82c6fc037f022cb7fe0f3f02ee8314d382a349b
sankarshan <ssankarshan636@gmail.com>
2025-05-06 08:17:38 +0530

    Remove SignUp component import from index.js to streamline exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,8 @@ import Header from './Header/Header.jsx';
 import Footer from './Footer/Footer.jsx';
 import Button from './Button.jsx'
 import Login from './Login.jsx';
-import SignUp from './SignUp.jsx';
+// import SignUp from './SignUp.jsx';
 import Logo from './Logo.jsx';
 import InputBox from './InputBox.jsx';
 import Select from './Select.jsx';
-export { Header, Footer, Button, Login, SignUp, Logo, InputBox, Select }; 
+export { Header, Footer, Button, Login, Logo, InputBox, Select }; 
